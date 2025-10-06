d7c3ca5aea2a2b084057e22671a02edfa6ea57a7
harmakanna <harmakanna@gmail.com>
2025-10-06 08:22:10 -0400

    Added missing files from previous commit

---
+++ MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Tink_a_Trainer;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Tink_a_Trainer.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
